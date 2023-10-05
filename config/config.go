618700d48acbcc166d6af710f3cae1905168bad9
RATIU5 <jcmemmott20@outlook.com>
2023-10-05 10:49:33 -0600

    edit: use go file config

--- config/config.go
+++ config/config.go
@@ -1,18 +1,7 @@
 package config
 
-import (
-	"fmt"
-
-	"github.com/spf13/viper"
-)
-
-func init() {
-	viper.SetConfigFile("config.yaml")
-	if err := viper.ReadInConfig(); err != nil {
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
-	}
-}
+var version = "0.1.0"
 
 func GetVersion() string {
-	return viper.GetString("version")
+	return version
 }
